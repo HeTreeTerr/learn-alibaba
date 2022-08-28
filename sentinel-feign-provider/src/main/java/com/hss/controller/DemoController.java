6ac0e6ba217a4e09fe844245f0317dbf73e897ce
‘HeTreeTerr’ <3110708879@qq.com>
2022-08-28 16:54:28 +0800

    sentinel 使用优化

--- sentinel-feign-provider/src/main/java/com/hss/controller/DemoController.java
+++ sentinel-feign-provider/src/main/java/com/hss/controller/DemoController.java
@@ -3,12 +3,17 @@ package com.hss.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class DemoController {
 
     @GetMapping(value = "/feign")
     public String feign(){
-
+        Integer ramdomNum = new Random().nextInt(10);
+        if(ramdomNum%2 == 0){
+            throw new RuntimeException();
+        }
         return "hello sentinel feign";
     }
 
